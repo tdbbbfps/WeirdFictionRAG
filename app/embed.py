26256bb8d62d195f79ab5cc2dcebe443c47e897c
tdbbb <tdbbb0427@gmail.com>
2025-08-18 21:52:26 +0800

    Update embed.py

--- app/embed.py
+++ app/embed.py
@@ -13,6 +13,8 @@ LLM_MODEL = "gemini-2.5-flash-lite"
 
 chromadb_client = chromadb.PersistentClient("./chroma.db")
 chromadb_collection = chromadb_client.get_or_create_collection("fiction")
+
+# 將文字轉換為向量陣列
 def embed(text : str, is_store : bool) -> list[float]:
     result = google_client.models.embed_content(
         model=EMBEDDING_MODEL,
@@ -24,7 +26,7 @@ def embed(text : str, is_store : bool) -> list[float]:
     assert result.embeddings
     assert result.embeddings[0].values # 為了方便直接用assert 實務上要做錯誤處理
     return result.embeddings[0].values
-
+# 創建向量資料庫
 def create_db() -> None:
     for idx, c in enumerate(chunk.get_chunks()):
         print(f"Process: {c}")
@@ -34,5 +36,29 @@ def create_db() -> None:
             documents=c,
             embeddings=embedding
         )
+# 查詢向量資料庫
+def query_db(question : str) -> list[str]:
+    question_embedding = embed(question, False)
+    result = chromadb_collection.query(
+        query_embeddings=question_embedding,
+        n_results=5 # 回傳5條最相關的內容
+    )
+    assert result["documents"]
+    return result["documents"][0]
+
 if __name__ == "__main__":
-    create_db()
+    question = input("請輸入你的問題：")
+    # create_db()
+    chunks = query_db(question)
+    prompt = "Please answer user's question accroding to context\n"
+    prompt += f"Question:{question}\n"
+    prompt += f"Context:"
+    for c in chunks:
+        prompt += f"{c}\n"
+        prompt += "-----\n"
+
+    result = google_client.models.generate_content(
+        model=LLM_MODEL,
+        contents=prompt
+    )
+    print(result)
